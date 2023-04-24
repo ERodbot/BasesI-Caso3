db00553c927513aea030abc0132ffb789d2eac06
DadoGC715 <jose.gutierrez.conejo@gmail.com>
2023-04-24 12:22:43 -0600

    next fills

---
+++ collectors fill.sql
@@ -0,0 +1,27 @@
+USE [Esencial Verde]
+
+-- alt_collectors fill --
+WITH number_sequence AS (
+  SELECT 1 as n
+  UNION ALL
+  SELECT n+1
+  FROM number_sequence
+  WHERE n < 1000
+)
+INSERT INTO alt_collectors (name, address_id)
+SELECT CONCAT('alt_collector', CEILING(RAND(CHECKSUM(NEWID())) * 1000) + 1), FLOOR(RAND(CHECKSUM(NEWID()))*1000)+1
+FROM number_sequence
+OPTION (MAXRECURSION 32767)
+
+--ev_collectors fill --
+WITH number_sequence AS (
+  SELECT 1 as n
+  UNION ALL
+  SELECT n+1
+  FROM number_sequence
+  WHERE n < 1000
+)
+INSERT INTO ev_collectors (name, ev_site_id)
+SELECT CONCAT('ev_collector', CEILING(RAND(CHECKSUM(NEWID())) * 1000) + 1), FLOOR(RAND(CHECKSUM(NEWID()))*1000)+1
+FROM number_sequence
+OPTION (MAXRECURSION 32767)
---
+++ producers fill.sql
@@ -0,0 +1,18 @@
+USE [Esencial Verde]
+
+--corporations fill --
+INSERT INTO corporations (name)
+VALUES('corp1');
+
+-- producers fill --
+WITH number_sequence AS (
+  SELECT 1 as n
+  UNION ALL
+  SELECT n+1
+  FROM number_sequence
+  WHERE n < 1000
+)
+INSERT INTO producers (name, env_score, corporation_id, address_id, computer, username, checksum, created_at, updated_at)
+SELECT CONCAT('producer', CEILING(RAND(CHECKSUM(NEWID())) * 1000) + 1), FLOOR(RAND(CHECKSUM(NEWID()))*100)+1, 1, FLOOR(RAND(CHECKSUM(NEWID()))*1000)+1, 'me', 'root', HASHBYTES('SHA2_256', 'hola mundo'), GETDATE(), GETDATE()
+FROM number_sequence
+OPTION (MAXRECURSION 32767)
